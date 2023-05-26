c9013b1a166406b969d6c9892f0086c302e919a6
Superluckyhu <Superluckyhu@163.com>
2023-05-26 11:35:09 +0800

    集成图标组件

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -46,9 +46,38 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <pre>
-        {{ebookList}}
-      </pre>
+      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
+        <template #footer>
+          <div>
+            <b>ant design vue</b>
+            footer part
+          </div>
+        </template>
+        <template #renderItem="{ item }">
+          <a-list-item key="item.title">
+            <template #actions>
+          <span v-for="{ type, text } in actions" :key="type">
+            <component v-bind:is="type" style="margin-right: 8px" />
+            {{ text }}
+          </span>
+            </template>
+            <template #extra>
+              <img
+                  width="272"
+                  alt="logo"
+                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+              />
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.title }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.avatar" /></template>
+            </a-list-item-meta>
+            {{ item.content }}
+          </a-list-item>
+        </template>
+      </a-list>
 
     </a-layout-content>
   </a-layout>
@@ -57,6 +86,7 @@
 <script lang="ts">
 import { defineComponent ,onMounted,ref } from 'vue';
 import axios from "axios";
+
 export default defineComponent({
   name: 'HomeView',
   components: {
